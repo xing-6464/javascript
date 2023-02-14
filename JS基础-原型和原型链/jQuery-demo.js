76e55b3f498ddf482bff3a9d4a4de0769fee9703
xing-6464 <xing_6464@foxmail.com>
2023-02-14 18:55:16 +0800

    更新

---
+++ JS基础-原型和原型链/jQuery-demo.js
@@ -0,0 +1,36 @@
+class jQuery {
+  constructor(selector) {
+    const result = document.querySelectorAll(selector)
+    const length = result.length
+    for (let i = 0; i < length; i++) {
+      this[i] = result[i]
+    }
+    this.length = length
+  }
+
+  get(index) {
+    return this[index]
+  }
+
+  each(fn) {
+    for (let i = 0; i < this.length; i++) {
+      const elem = this[i]
+      fn(elem)
+    }
+  }
+
+  on(type, fn) {
+    return this.each((elem) => {
+      elem.addEventListener(type, fn, false)
+    })
+  }
+
+  // 扩展很多DOM API
+}
+
+// 添加插件
+jQuery.prototype.dialog = function (info) {
+  alert(info)
+}
+
+// "造轮子" 使用继承
