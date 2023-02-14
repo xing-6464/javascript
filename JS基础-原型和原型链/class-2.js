12447399bac15ed901427dda73102bc9091a482a
xing-6464 <xing_6464@foxmail.com>
2023-02-14 18:44:36 +0800

    原型

--- JS基础-原型和原型链/class-2.js
+++ JS基础-原型和原型链/class-2.js
@@ -44,3 +44,12 @@ console.log(wanglaoshi.name)
 console.log(wanglaoshi.major)
 wanglaoshi.teach()
 wanglaoshi.eat()
+
+// 原型
+typeof People // 'function'
+typeof Student // 'function'
+
+// 隐式原型和显式原型
+console.log(xialuo.__proto__)
+console.log(Student.prototype)
+console.log(xialuo.__proto__ === Student.prototype)
